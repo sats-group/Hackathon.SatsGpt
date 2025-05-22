ae35ce4cd97223be7d1dde868f47b7fca0b4f62a
Johan Sørensen <johan@johansorensen.com>
2025-05-22 09:32:35 +0200

    Tailwind and chats layout

--- web/app/routes.ts
+++ web/app/routes.ts
@@ -1,10 +1,10 @@
-import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  ...prefix("chats", [
-    index("routes/chats/index.tsx"),
+  layout("routes/chats/layout.tsx", [
+    route("chats", "routes/chats/index.tsx"),
     //route("new", "routes/chats/new.tsx"),
-    route(":chatId", "routes/chats/chat.tsx"),
+    route("chats/:chatId", "routes/chats/chat.tsx"),
   ]),
 ] satisfies RouteConfig;
